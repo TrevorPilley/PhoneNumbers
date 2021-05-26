316e3dafab199a0d54556ee0c0c67ac30e97b71d
Trevor Pilley <TrevorPilley@users.noreply.github.com>
2021-05-26 11:47:56 +0100

    Fix tests

--- src/PhoneNumbers/CountryInfo_Countries.cs
+++ src/PhoneNumbers/CountryInfo_Countries.cs
@@ -37,7 +37,8 @@ namespace PhoneNumbers
             CallingCode = "+49",
             Iso3166Code = "DE",
             Name = "Germany",
-            NsnLengths = new ReadOnlyCollection<int>(new[] { 12 }),
+            NdcLengths = new ReadOnlyCollection<int>(new[] { 2 }),
+            NsnLengths = new ReadOnlyCollection<int>(new[] { 11, 12 }),
             TrunkPrefix = "0",
         };
 
